c55a66d60a03b6b96a1cc0457cf502675ef95b40
JoonLind <84848676+JoonLind@users.noreply.github.com>
2022-01-19 09:36:29 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?phpsession_start();
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="app.css">
+    </head>
+<body>
+
+<h1>Kirjautuminen</h1>
+
+<form method="POST" action="kirjaudu.php">
+    <p><input type="email" name="email" placeholder="Sähköposti" required></p>
+
+
+
+<?php
+
+if(isset($_SESSION["email_error"]))
+
+?>
+
+<p><input type="password" name="password" placeholder="Salasana" required></p>
+    <p><button type="submit">Kirjaudu</button></p>
+</form>
+
+</body>
+</html>
